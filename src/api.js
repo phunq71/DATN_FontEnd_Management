f83ad38bf58bb9a4f921fdc4d85bdb96ac55eeb4
chistaam <tampcps38349@gmail.com>
2025-07-24 22:41:05 +0700

    login and isLogin

--- src/api.js
+++ src/api.js
@@ -3,38 +3,46 @@
 
 api.interceptors.request.use(
     (config) => {
-        console.log("==== INTERCEPTOR RUNNING ====");
-        console.log("Request URL:", config.url);
 
         // Mấy đường này KHÔNG gắn token
         const skipAuthUrls = ["/auth/admin/login", "/auth/refresh"];
-        console.log("Skip Auth URLs:", skipAuthUrls);
+        //console.log("Skip Auth URLs:", skipAuthUrls);
 
         const isSkip = skipAuthUrls.some((url) =>
             config.url.includes(url)
         );
-        console.log("Is Skip:", isSkip);
+        ////console.log("Is Skip:", isSkip);
 
         if (!isSkip) {
-            const token = localStorage.getItem("accessToken");
-            console.log("Got Token From LocalStorage:", token);
+            const token = getCookie("accessToken");
+            //console.log("Got Token From LocalStorage:", token);
 
             if (token) {
                 config.headers.Authorization = `Bearer ${token}`;
-                console.log("Added Authorization Header:", config.headers.Authorization);
+                //console.log("Added Authorization Header:", config.headers.Authorization);
             } else {
-                console.log("No token found -> No Authorization header added");
+                //console.log("No token found -> No Authorization header added");
             }
         } else {
-            console.log("This URL is in skip list -> No Authorization header added");
+            //console.log("This URL is in skip list -> No Authorization header added");
         }
 
-        console.log("==== INTERCEPTOR DONE ====");
+        //console.log("==== INTERCEPTOR DONE ====");
         return config;
     },
     (error) => {
-        console.log("INTERCEPTOR ERROR:", error);
+        //console.log("INTERCEPTOR ERROR:", error);
         return Promise.reject(error);
     }
 );
+
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift();
+    }
+    return null; // không tìm thấy thì trả về null
+}
 
