74007752edae1635de2ac76357c709a33ed340c9
diepmthuan <thuandmps38008@gmail.com>
2025-08-19 14:19:43 +0700

    Final Quản lý khách hàng

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import StaffManagement from "../components/StaffManagement.vue";
 
 
 import Category from '../components/Category.vue';
+import CustomerManagement from "../components/CustomerManagement.vue";
 const routes = [
     {
         path: '/',
@@ -68,6 +69,11 @@ const routes = [
     {
         path: '/OrderManagement',
         component: OrderManagement,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/CustomerManagement',
+        component: CustomerManagement,
         meta: { requiresAuth: true }
     },
 ]
